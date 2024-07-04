40efdfd8a4cd1bf1b515251264aa45a2223b8137
Siyasanga <siyasanga84@gmail.com>
2024-07-04 20:27:45 +0200

    Insert first project

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import speed from '../assets/speed.png'
 
 const Projects = () => {
   return (
     <div className='project'>
     <h1>Projects</h1> 
+    <div className='card'>
+      <p>Internet speed test built with python and the tkinter GUI library</p>
+      <img src={speed} alt="internet speed test"/>
+    </div>
     </div>
   )
 }
