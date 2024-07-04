151bc69871b7f559a8ad6f248fca148e763b65f7
Siyasanga <siyasanga84@gmail.com>
2024-07-04 14:36:38 +0200

    create class names for divs and move styles to seperate css

--- src/components/container.jsx
+++ src/components/container.jsx
@@ -2,35 +2,13 @@ import  './container.css'
 
 const SplitContainer = ({ leftContent, rightContent }) => {
   return (
-    <div style={{
-      display: 'flex',
-      width: '100%',
-      height: '100%',
-      maxWidth: '1200px',
-      margin: '0 auto',
-    }}>
-      <div style={{
-        flex: 1,
-        color: 'white',
-        padding: '20px', 
-      }}>
+    <div className="splitContainer">
+      <div className="leftContainer">
         {leftContent}
       </div>
-      <div style={{
-        flex: 1,
-        color: 'white',
-        padding: '20px',
-        overflowY: 'scroll',
-        maxHeight: '100%',
-        marginTop: '50px',
-        scrollbarWidth: 'none',  // Firefox
-        msOverflowStyle: 'none',  // IE 10+
-      }}>
-        <div style={{
-          height: '100%',
-          paddingRight: '17px',  // Compensate for scrollbar width
-          boxSizing: 'content-box',  // Include padding in element's total width
-        }}>
+
+      <div className="rightContainer" >
+        <div className='rightContent'>
         {rightContent}
         </div>
       </div>
